19e5ad0efbbd0acfd96431f82030fb37ea826ec0
eightpigs <isme@lyinlong.me>
2016-10-30 01:00:05 +0800

    添加选择日期

--- app/src/main/java/me/lyinlong/taskline/MainActivity.java
+++ app/src/main/java/me/lyinlong/taskline/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import me.lyinlong.taskline.utils.InterfaceUtils;
 import me.lyinlong.taskline.utils.TimeUtils;
 import me.lyinlong.taskline.widget.calendarview.DateUtils;
 import me.lyinlong.taskline.widget.mdtabs.MyFragment;
@@ -20,17 +22,47 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private TabLayout tabLayout;
+    // tabs对应的内容
     private ViewPager viewPager;
-    private NestedScrollView nsvBody;
+
+    // 当前日期
+    private TextView mtvNowDateTime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mtvNowDateTime = (TextView)findViewById(R.id.tvNowDateTime);
+
+        String time = TimeUtils.getNowTime("yyyy年MM月");
+        // 设置当前时间
+        mtvNowDateTime.setText(time);
+
+        // 初始化布局
+        initView();
+
+        //事件
+        mtvNowDateTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InterfaceUtils.show_cusAlertDialog(MainActivity.this, R.layout.layout_date_choose ,null ,null,true , true , 6);
+            }
+        });
+
+    }
+
+    /**
+     * 初始化布局
+     */
+    private void initView(){
+        // 设置工具类
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
+        // 禁用返回按钮
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -44,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
 
         // 默认选中指定日期
         tabLayout.getTabAt(TimeUtils.getNowDay()).select();
-
     }
+
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
